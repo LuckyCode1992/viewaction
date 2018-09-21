b7e1a7045809cddc585b8b0f6c1b0d3916419ee9
2214576751@qq.com <hxl19920401>
2018-09-21 17:04:08 +0800

    同步代码

--- app/src/main/java/com/justcode/hxl/viewaction/path/gouwuche/BezierTypeEvaluator.java
+++ app/src/main/java/com/justcode/hxl/viewaction/path/gouwuche/BezierTypeEvaluator.java
@@ -17,7 +17,7 @@ public class BezierTypeEvaluator implements TypeEvaluator<PointF> {
     public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
         PointF pointCur = new PointF();
         /**
-         * 这是三阶贝塞尔曲线公式，就是二次函数的公式
+         * 这是三阶贝塞尔曲线公式
          */
         pointCur.x = (1 - fraction) * (1 - fraction) * startValue.x + 2 * fraction * (1 - fraction) * mControllPoint.x + fraction * fraction * endValue.x;
         pointCur.y = (1 - fraction) * (1 - fraction) * startValue.y + 2 * fraction * (1 - fraction) * mControllPoint.y + fraction * fraction * endValue.y;
