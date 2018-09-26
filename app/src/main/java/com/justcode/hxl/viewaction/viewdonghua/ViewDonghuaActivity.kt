0734afa96d570032721c9cb3032f7897b3be7c37
2214576751@qq.com <hxl19920401>
2018-09-26 15:02:10 +0800

    同步代码

--- app/src/main/java/com/justcode/hxl/viewaction/viewdonghua/ViewDonghuaActivity.kt
+++ app/src/main/java/com/justcode/hxl/viewaction/viewdonghua/ViewDonghuaActivity.kt
@@ -13,15 +13,15 @@ class ViewDonghuaActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_donghua)
 
         val picData = arrayListOf(
-                "https://ws1.sinaimg.cn/large/0065oQSqly1fvexaq313uj30qo0wldr4.jpg",
-                "https://ws1.sinaimg.cn/large/0065oQSqly1fv5n6daacqj30sg10f1dw.jpg",
-                "https://ws1.sinaimg.cn/large/0065oQSqly1fuo54a6p0uj30sg0zdqnf.jpg",
-                "https://ws1.sinaimg.cn/large/0065oQSqly1fuh5fsvlqcj30sg10onjk.jpg",
-                "https://ws1.sinaimg.cn/large/0065oQSqly1fubd0blrbuj30ia0qp0yi.jpg",
-                "https://ww1.sinaimg.cn/large/0065oQSqly1fu7xueh1gbj30hs0uwtgb.jpg",
-                "https://ww1.sinaimg.cn/large/0065oQSqgy1fu39hosiwoj30j60qyq96.jpg",
-                "https://ww1.sinaimg.cn/large/0065oQSqly1ftzsj15hgvj30sg15hkbw.jpg",
-                "https://ww1.sinaimg.cn/large/0065oQSqgy1ftwcw4f4a5j30sg10j1g9.jpg"
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910",
+                "测试数据12345678910"
 
         )
 
