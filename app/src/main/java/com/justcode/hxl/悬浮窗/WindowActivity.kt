1eb001a3bbf1eac765e76352cd3631d8d4f51bfb
2214576751@qq.com <hxl19920401>
2018-10-09 14:47:36 +0800

    优化悬浮窗

--- app/src/main/java/com/justcode/hxl/悬浮窗/WindowActivity.kt
+++ app/src/main/java/com/justcode/hxl/悬浮窗/WindowActivity.kt
@@ -29,20 +29,29 @@ class WindowActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
 
+        shouquan.setOnClickListener {
+            //启动Activity让用户授权
+            val intent = Intent()
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.setData(Uri.parse("package:$packageName"))
+            startActivityForResult(intent, 100)
+        }
         start_float_window.setOnClickListener {
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (Settings.canDrawOverlays(applicationContext)) {
-                    startService2Window()
-                } else {
-                    //启动Activity让用户授权
-                    val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-                    intent.setData(Uri.parse("package:$packageName"))
-                    startActivityForResult(intent, 100)
-                }
-            } else {
-                startService2Window()
-            }
+            startService2Window()
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                if (Settings.canDrawOverlays(applicationContext)) {
+//                    startService2Window()
+//                } else {
+//                    //启动Activity让用户授权
+//                    val intent = Intent()
+//                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//                    intent.setData(Uri.parse("package:$packageName"))
+//                    startActivityForResult(intent, 100)
+//
+//                }
+//            } else {
+//                startService2Window()
+//            }
         }
 
 
@@ -52,48 +61,6 @@ class WindowActivity : AppCompatActivity() {
         val intent = Intent(this, FloatWindowService::class.java)
         startService(intent)
         FloatWindowService.activity = this
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    fun showWindow() {
-        //Activity中的方法,得到窗口管理器
-//        mWindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-//        //设置悬浮窗布局属性
-//        val mWindowLayoutParams = WindowManager.LayoutParams()
-//        //设置类型,具体有哪些值可取在后面附上
-//        if (Build.VERSION.SDK_INT >= 26) {//8.0新特性
-//            mWindowLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-//        } else {
-//            mWindowLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
-//        }
-//
-//        //设置行为选项,具体有哪些值可取在后面附上
-//        mWindowLayoutParams.flags = (WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or WindowManager.LayoutParams.FLAG_BLUR_BEHIND
-//                or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
-//        //设置悬浮窗的显示位置
-//        mWindowLayoutParams.gravity = Gravity.LEFT or Gravity.TOP
-//        //        //设置悬浮窗的横竖屏,会影响屏幕方向,只要悬浮窗不消失,屏幕方向就会一直保持,可以强制屏幕横屏或竖屏
-//        //        mWindowLayoutParams.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-//        //设置 x 轴的偏移量
-//        mWindowLayoutParams.x = 0
-//        //设置 y 轴的偏移量
-//        mWindowLayoutParams.y = 0
-//        //如果悬浮窗图片为透明图片,需要设置该参数为 PixelFormat.RGBA_8888
-//        mWindowLayoutParams.format = PixelFormat.RGBA_8888
-//        //设置悬浮窗的宽度
-//        mWindowLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT
-//        //设置悬浮窗的高度
-//        mWindowLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
-//        //设置悬浮窗的布局
-//        floatWindowView = LayoutInflater.from(this).inflate(R.layout.float_window, null)
-//        //加载显示悬浮窗
-//        mWindowManager?.addView(floatWindowView, mWindowLayoutParams)
-//        floatWindowView?.setOnTouchListener(object : View.OnTouchListener{
-//            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-//              return false
-//            }
-//
-//        })
     }
 
     override fun onDestroy() {
