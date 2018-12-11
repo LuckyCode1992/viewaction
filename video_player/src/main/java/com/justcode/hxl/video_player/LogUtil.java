57d8c6919c43c3b35e77185a9caf23450a8883f3
2214576751@qq.com <hxl19920401>
2018-12-11 17:16:05 +0800

    视频播放功集成

---
+++ video_player/src/main/java/com/justcode/hxl/video_player/LogUtil.java
@@ -0,0 +1,23 @@
+package com.justcode.hxl.video_player;
+
+import android.util.Log;
+
+/**
+ * log工具.
+ */
+public class LogUtil {
+
+    private static final String TAG = "NiceVideoPlayer";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+
+    public static void i(String message) {
+        Log.i(TAG, message);
+    }
+
+    public static void e(String message, Throwable throwable) {
+        Log.e(TAG, message, throwable);
+    }
+}
