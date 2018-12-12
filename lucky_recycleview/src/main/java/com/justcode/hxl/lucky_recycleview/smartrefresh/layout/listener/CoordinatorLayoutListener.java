57b46a3a771133dcf70a8703d3c94d68a5285035
2214576751@qq.com <hxl19920401>
2018-12-12 16:38:28 +0800

    recycleview的refresh和loadmore完成

---
+++ lucky_recycleview/src/main/java/com/justcode/hxl/lucky_recycleview/smartrefresh/layout/listener/CoordinatorLayoutListener.java
@@ -0,0 +1,5 @@
+package com.justcode.hxl.lucky_recycleview.smartrefresh.layout.listener;
+
+public interface CoordinatorLayoutListener {
+    void onCoordinatorUpdate(boolean enableRefresh, boolean enableLoadMore);
+}
