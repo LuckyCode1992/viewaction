77acd29ef6e939e194eafa6305869550d0babe6c
2214576751@qq.com <hxl19920401>
2018-12-12 17:22:08 +0800

    recycleview持续提交

---
+++ lucky_recycleview/src/main/java/com/justcode/hxl/lucky_recycleview/luckycycleview/ViewHolder.kt
@@ -0,0 +1,6 @@
+package com.justcode.hxl.lucky_recycleview.luckycycleview
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
