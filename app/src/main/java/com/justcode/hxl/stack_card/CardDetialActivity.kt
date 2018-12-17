bb0dbcd8f664ae62c52d6962e7045c138f3011be
2214576751@qq.com <hxl19920401>
2018-12-17 09:54:22 +0800

    同步代码

--- app/src/main/java/com/justcode/hxl/stack_card/CardDetialActivity.kt
+++ app/src/main/java/com/justcode/hxl/stack_card/CardDetialActivity.kt
@@ -3,10 +3,19 @@ package com.justcode.hxl.stack_card
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.justcode.hxl.viewaction.R
+import kotlinx.android.synthetic.main.activity_card_detial.*
+
 class CardDetialActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_detial)
+        val title = intent.getStringExtra("title")
+        val loggo = intent.getIntExtra("logo",0)
+        val background = intent.getIntExtra("background",0)
+
+        relative_layout.setBackgroundResource(background)
+        logo_image_view.setImageResource(loggo)
+        title_text_view.text = title
     }
 }
